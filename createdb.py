19e8cd856f2c32b81283286d95c1c63711181254
nnll2 <jinhuaxing@gmail.com>
2016-09-29 17:05:49 +0800

    merge create valuelog

--- createdb.py
+++ createdb.py
@@ -2,8 +2,10 @@ import os
 import sqlite3
 
 from invests import sql_create_invest
+from invests import sql_create_valuelog
 
 os.system("del mim.db")
 conn = sqlite3.connect("mim.db")
 with conn:
   conn.execute(sql_create_invest)
+  conn.execute(sql_create_valuelog)
